a5b415588229d4e90c9fb635b8dacd98bf693fe3
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-06 07:17:14 +0530

    edited4 code

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=Suersh
 
-echo "$(PERSON1): Hi $(PERSON2)"
-echo "$(PERSON2): Hi $(PERSON1) How are you"
-echo "$(PERSON1): Good What about you!"
-echo "$(PERSON2): Fine"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1 How are you"
+echo "$PERSON1: Good What about you!"
+echo "$PERSON2: Fine"
