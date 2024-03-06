bfdeb432123120e0e5f9d811fcc1439e8d5c9892
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-06 08:01:10 +0530

    Edited05

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-echo"Please enter your user name"
+echo " Please enter your user name "
 
 read USERNAME
 
-echo"Please enter your passward"
+echo " Please enter your passward "
 
 read PASSWORD
 
-echo"User name is :$USERNAME , Password is :$PASSWORD"
+echo " User name is: $USERNAME , Password is: $PASSWORD "
