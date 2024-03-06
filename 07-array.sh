487e1f0cfd392076491f4e8d115d1004f78c7c07
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-06 10:18:29 +0530

    Edited10

---
+++ 07-array.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+Fruits=("Mango" "Apple" "Banana")
+
+echo "Fisrt fruit is : ${Fruits[0]}
+echo "Second fruit is : ${Fruits[1]}
+echo "Thrid fruit is : ${Fruits[2]}
+echo "All fruit are : ${Fruits[@]}
