13f9ef1aa086481de2ba909e338031f88a1293f8
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-07 07:08:19 +0530

    Edited12

--- 08-condtions.sh
+++ 08-condtions.sh
@@ -6,6 +6,5 @@ if [NUMBER -ge 100]
 then
     echo "given number is $NUMBER greterthen 100"
 else
-    echo given number is $NUMBER lessthen 100"
+    echo "given number is $NUMBER lessthen 100"
 fi
-
