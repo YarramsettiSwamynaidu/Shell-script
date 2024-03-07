b5852fd77bcca53a113aa3b2e6d5f849d431c2b8
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-07 18:50:08 +0530

    Edited16

---
+++ 10-functions.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+ruser=$(id -u)
+
+VALIDATE {
+
+if [ $? -ne 0 ]
+then
+    echo "installation of Package is failed"
+    exit 1
+else
+    echo "innstallation of Package is sucess"
+fi
+
+}
+
+if [ $ruser -ne 0 ]
+then
+    echo "Plesae run command with root user"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
+
+yum install nginx -y
+
+VALIDATE
