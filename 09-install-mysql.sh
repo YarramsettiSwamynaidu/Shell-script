8e08b769e184db6a71e5a877c7f476e38f525f3e
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-07 18:16:38 +0530

    Edited15

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -4,7 +4,9 @@ ruser=$(id -u)
 
 if [ $ruser -ne 0 ]
 then
-    echo "plesae run command with root user"
+    echo "Plesae run command with root user"
 else
-    yum install mysql -y
+    echo "You are root user"
 fi
+
+yum install mysql -y
