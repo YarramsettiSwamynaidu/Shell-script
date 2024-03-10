b6bf083746a8f47261137c4b8be42b7daba8617f
swamydevops <swamynaiduyarramsetti@gmail.com>
2024-03-10 11:34:48 +0530

    adding comment

---
+++ 15-user-info.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+Filename="/etc/passwd"
+
+if [ ! -f $Filename ]
+then
+    echo -e "Soucce Diectory: $Filename \e[31m dirctory not existed"
+fi
+
+
+while IFS= ":"read -r Username Password user_id group_id
+do
+    echo "Username:$Username"
+    echo "Password:$Password"
+    echo "User id:$user_id"
+    echo "Group id:$group_id"
+
+done <<< $Filename
